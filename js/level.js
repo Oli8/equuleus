10d66dc4e5124b39212e4bd08449d2af900d9a61
Olivier Crété <ocrete@sfrpims.fr>
2017-12-29 16:45:16 +0100

    level class

---
+++ js/level.js
@@ -0,0 +1,17 @@
+class Level {
+
+	constructor(title, author, tiles){
+		this.title = title;
+		this.author = author;
+		this.tiles = tiles;
+	}
+
+}
+
+/*
+l = new Level("test", "Oli", []);
+
+function c(v){
+	console.log(v);
+}
+*/
