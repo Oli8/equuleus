13b6d0a759935145e3849f335fe4480b8da4a53b
Olivier Crété <ocrete@sfrpims.fr>
2018-03-08 18:34:13 +0100

    Box level smaller

--- js/levels.js
+++ js/levels.js
@@ -1,15 +1,13 @@
 const levels = [
 	new Level("It's a Box's Box's World", "Oli", [
-		["rock", "rock", "rock", "rock", "water", "water", "water", "water", "water"],
-		["rock", "start", "exit", 0, "rock", "water", "water", "water", "water"],
-		["rock", 0, "box", "box", "rock", "water", "rock", "rock", "rock"],
-		["rock", 0, "box", 0, "rock", "water", "rock", "boxSpot", "rock"],
-		["rock", "rock", "rock", 0, "rock", "rock", "rock", "boxSpot", "rock"],
-		["water", "rock", "rock", 0, 0, 0, 0, "boxSpot", "rock"],
-		["water", "rock", 0, 0, 0, "rock", 0, 0, "rock"],
-		["water", "rock", 0, 0, 0, "rock", "rock", "rock", "rock"],
-		["water", "rock", "rock", "rock", "rock", "water", "water", "water", "water"]
-	], 9, 9, "Easy"),
+		["start", 0, 0, "rock", "water", "water", "water"],
+		[0, "box", "box", "rock", "water", "rock", "rock"],
+		[0, "box", 0, "rock", "water", "rock", "boxSpot"],
+		["rock", "rock", 0, "rock", "rock", "rock", "boxSpot"],
+		["rock", "rock", 0, 0, 0, 0, "boxSpot"],
+		["rock", 0, 0, 0, "rock", "exit", 0],
+		["rock", 0, 0, 0, "rock", "rock", "rock"],
+	], 7, 7, "Easy"),
 	new Level("Over the Briges", "Oli", [
 		["start", 0, 0],
 		[0, "box", 0],
